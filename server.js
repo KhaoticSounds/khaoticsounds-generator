8a56f116e9cda801126c526d790a0d5d90bbdb6a
KhaoticSounds <65625203+KhaoticSounds@users.noreply.github.com>
2025-05-08 15:18:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const path = require('path');
-const cors = require('cors');
-
-app.use(cors());
-app.use(express.static(path.join(__dirname)));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+res.sendFile(path.join(__dirname, 'index.html'));
