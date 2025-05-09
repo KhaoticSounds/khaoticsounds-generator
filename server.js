b05945837576a0be836e7233071b7e506a207f38
KhaoticSounds <65625203+KhaoticSounds@users.noreply.github.com>
2025-05-09 17:48:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.post('/api/generate', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
